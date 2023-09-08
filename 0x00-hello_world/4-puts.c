b1b3670b7c9873a25ca8c1b761b3047a8e93d662
kefilweditse <kefilwestreets@gmail.com>
2023-09-07 19:15:03 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main -  entry point
+*puts - display text
+*return : always 0 (correct)
+*/
+int main(void)
+{
+	puts("\Programming is like building a multilingual puzzle");
+	return (0);
+}
