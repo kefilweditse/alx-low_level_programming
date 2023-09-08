4c368d16865bddba01e52446dc5beab87d0c73ce
kefilweditse <kefilwestreets@gmail.com>
2023-09-07 20:08:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main -  entry point
+*main - entry point
 *puts - display text
 *return : always 0 (correct)
 */
 int main(void)
 {
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
