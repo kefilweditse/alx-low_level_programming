4f0eadb36096f8eebc7ee44ebc85dfce36def532
kefilweditse <kefilwestreets@gmail.com>
2023-09-11 11:08:27 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed bt newline ,error.
+ * Return:  1.
+ */
+
+int main(void)
+
+{
+	write( 2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1);
+
+}
