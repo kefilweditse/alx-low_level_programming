fccf37e7b4868a6f4f1c7b04253cef62110e549b
kefilweditse <kefilwestreets@gmail.com>
2023-09-11 07:18:31 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point.
+ * Return: 0 (success0
+ */
+
+int main(void)
+
+{
+
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+
+}
