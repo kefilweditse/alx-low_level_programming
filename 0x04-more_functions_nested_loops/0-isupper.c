ecaf2366dd6434907d47b1e5937f4b32bc2de390
kefilweditse <kefilwestreets@gmail.com>
2023-09-15 23:17:48 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Check Upper letter
+ * @i: The number to be checked
+ * Return: 1 for upper or 0 for any
+ */
+
+int _isupper(int i)
+
+{
+
+	if (i >= 65 && <+ 90)
+	{
+	return(1);
+	}
+	return(0);
+}
